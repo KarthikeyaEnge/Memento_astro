f818ec7b8f675199b655a037d9917751e15fc666
Enge Karthikeya <karthikeya.enge@hotmail.com>
2023-08-31 00:48:24 +0530

    optimized the images sizes for better performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import image from "@astrojs/image";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), image()],
+  integrations: [react(), tailwind()],
   vite: {
     ssr: {
       noExternal: ["react-icons"],
