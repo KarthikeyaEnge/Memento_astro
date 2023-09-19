5d3ce8657c46492f6da77da9fb1efd49c41c51f0
Enge Karthikeya <karthikeya.enge@hotmail.com>
2023-09-19 18:53:58 +0530

    added projects page

---
+++ src/data/projects.js
@@ -0,0 +1,53 @@
+const projects = {
+  data: [
+    {
+      name: "MINESKILL",
+      img: "https://raw.githubusercontent.com/KarthikeyaEnge/Memento/main/src/assets/ms.png",
+      id: "2023",
+      gitr: "https://github.com/KarthikeyaEnge/StudyScraper_FrontEnd",
+      desc: `A web App that helps students to find quality content in web, videos
+        with just their syllabus copy scan/photo. Just upload the syllabus copy
+        you can get quality content like videos, website-links and more.`,
+      pro_link: "https://mineskill.onrender.com/",
+      stack: [
+        "ReactJS",
+        "ViteJS",
+        "NodeJS",
+        "ExperssJS",
+        "MongoDB",
+        "TailwindCSS",
+      ],
+    },
+    {
+      name: "TAKHALI TIMER",
+      img: "/images/takhali.png",
+      id: "2023",
+      gitr: "https://github.com/KarthikeyaEnge/Takhali_Pomodoro",
+      desc: `A Pomodoro timer web app & PWA to focus and increase your productivity`,
+      pro_link: "https://takhali.onrender.com/",
+      stack: ["ReactJS", "ViteJS", "TailwindCSS", "DaisyUI", "Chartjs"],
+    },
+    {
+      name: " ATTENDANCE_MANAGER",
+      img: "https://raw.githubusercontent.com/KarthikeyaEnge/Memento/main/src/assets/am.png",
+      id: "2023",
+      gitr: "https://github.com/KarthikeyaEnge/Attendance_mng",
+      desc: `A web application which basically ease daily attendance procedure and has features like attendance pdf and excel sheet converter.`,
+      pro_link: "https://github.com/KarthikeyaEnge/Attendance_mng",
+      stack: ["ReactJS", "ViteJS", "Google Scripts", "TailwindCSS", "DaisyUI"],
+    },
+    {
+      name: "RAILRESERVE SYS.",
+      img: "https://raw.githubusercontent.com/KarthikeyaEnge/Memento/main/src/assets/rs.png",
+      id: "2022",
+      gitr: "https://github.com/KarthikeyaEnge/Learn-web_progs/tree/main/web_adv/webpage_15",
+      desc: `A web App that acts as a platform for Railway Reservation using user
+        authentication, and features like past reservation search and more.`,
+      pro_link:
+        "https://github.com/KarthikeyaEnge/Learn-web_progs/tree/main/web_adv/webpage_15",
+      stack: ["HTML", "CSS", "Python", "Django", "Javascript", "SQLite"],
+    },
+  ],
+};
+
+export default projects;
